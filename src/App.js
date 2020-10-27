8dcb7f71115ffe4ace9500369f1a92140008f06c
Samantha Thompson <samanthathompson521@gmail.com>
2020-10-27 01:44:26 -0400

    Initial work experience section

--- src/App.js
+++ src/App.js
@@ -9,25 +9,27 @@ import Home from './components/Home.jsx';
 import AboutMe from './components/AboutMe.jsx';
 import Contact from './components/Contact.jsx';
 import Loader from './components/Loader.jsx';
+import Experience from './components/Experience.jsx';
 
 function App() {
   const [Loading, setLoading] = useState(true);
   
   setTimeout(()=>{
-    setLoading(false);
-  }, 2000
+      setLoading(false);
+    }, 2000
   );
   
   return (
     
     <div className={Loading ? 'App overflow-hidden' : 'App'}>
-      <Loader Loading={Loading}></Loader>
-      <Header></Header>
-      <Footer></Footer>
-      <Home></Home>
-      <AboutMe></AboutMe>
-      <Projects></Projects>
-      <Contact></Contact>
+      <Loader Loading={Loading} />
+      <Header />
+      <Footer />
+      <Home />
+      <AboutMe />
+      <Projects />
+      <Experience />
+      <Contact />
       
     </div>
   );
---
+++ src/components/Experience.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import work from './work.json';
+
+import '../styles/Experience.css';
+
+const Experience = () => {
+  const [selected, setSelected] = useState(work[0]);
+
+  const workList = work.map((elm) => {
+    const className = elm.id === selected.id ? 'selected' : '';
+    return (
+      <div key={elm.id} className={className}>
+        <button type="button" className="btn click" onClick={() => { setSelected(elm); }}>
+          <span>{elm.company}</span>
+        </button>
+      </div>
+    );
+  });
+
+  const workBullets = selected.description.map((elm, key) => (
+    <li key={`bullet${key}`}>
+      {elm}
+    </li>
+  ));
+
+  return (
+    <div id="experience" className="break mx-2 section">
+      <Container fluid="md" className="test">
+        <h2 className="mb-4 w-100 dotted-line">Experience<span className="dot"></span></h2>
+        <Row>
+          <Col md={4} lg={3} className="py-2">
+            <div className="buttonContainer">
+              {workList}
+            </div>
+          </Col>
+          <Col md={8} lg={9} className="py-2">
+            <h4>
+              <span>{selected.company}</span>
+              {' '}
+              -
+              {' '}
+              <span>{selected.position}</span>
+            </h4>
+            <p>{selected.dates}</p>
+            <div>
+              <ul>
+                {workBullets}
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Experience;
