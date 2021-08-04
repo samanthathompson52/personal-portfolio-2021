bf78f9d39f78f61eb41fd21f2a4df3a39ecbb520
Samantha Thompson <samanthathompson521@gmail.com>
2021-08-04 19:18:29 -0400

    Added pediatric website project information

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/Projects.css';
 import discord from '../pictures/discord-twitch-bot.png';
+import pediatrics from '../pictures/pediatrics.png';
+
 
 import Container from 'react-bootstrap/Container'
 
@@ -10,26 +12,49 @@ const Projects = () => {
     <div className = 'section'>
       <Container fluid className="text-left">
       <h4>Projects</h4>
-       <div className = "row">
-      <div className="col-sm-8"> 
-        <h6 className = 'w-100 dotted-line'>Twitch Community Discord Bot<span className="dot"></span></h6>
-         <h5>Created a custom discord bot that serves as a management and entertainment tool for over 300 people in a Twitch community. Features include automatic role assigment, queue system, custom welcome messages, and role assigment based upon user reactions.  </h5>
-         <div className = 'tech-list'>
-         <ul className="list-inline ul">
-           <li className="list-inline-item li">Python</li>
-           <li className="list-inline-item li">Discord API</li>
-           </ul>
-           </div>
+      <div>
+        <div className = "row">
+          <div className="col-sm-8"> 
+            <h6 className = 'w-100 dotted-line'>Twitch Community Discord Bot<span className="dot"></span></h6>
+            <h5>Created a custom discord bot that serves as a management and entertainment tool for over 300 people in a Twitch community. Features include automatic role assigment, queue system, custom welcome messages, and role assigment based upon user reactions.  </h5>
+            <div className = 'tech-list'>
+              <ul className="list-inline ul">
+              <li className="list-inline-item li">Python</li>
+              <li className="list-inline-item li">Discord API</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-sm-4"> 
+          <div className = "project-files">
+            <a href="https://github.com/samanthathompson52/discord-bot" target="_blank" rel="noopener noreferrer"><img src={discord} width = "60%" alt="Link to Github for Twitch Community Discord Bot" /></a>
+          </div>  
+        </div>
       </div>
-      <div className="col-sm-4"> 
-        <div className = "project-files">
-           <a href="https://github.com/samanthathompson52/discord-bot" target="_blank" rel="noopener noreferrer"><img src={discord} width = "80%" alt="Link to Twitch Discord Bot project file" /></a>
-        </div>  
 
-      </div>
-      </div>
+      <div>
+        <div className = "row">
+          <div className="col-sm-8"> 
+            <h6 className = 'w-100 dotted-line'>Pediatrics Office Website<span className="dot"></span></h6>
+            <h5>Designed and built an original website for a local pediatric practice.  The website features information on both of their locations, services, staff and medical education documents.  </h5>
+            <div className = 'tech-list'>
+              <ul className="list-inline ul">
+              <li className="list-inline-item li">HTML</li>
+              <li className="list-inline-item li">CSS</li>
+              <li className="list-inline-item li">React</li>
+              <li className="list-inline-item li">Javascript</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-sm-4"> 
+          <div className = "project-files">
+            <a href="https://github.com/samanthathompson52/discord-bot" target="_blank" rel="noopener noreferrer"><img src={pediatrics} width = "60%" alt="Link to Github for Pediatrics Website" /></a>
+          </div>  
+          </div>  
+        </div>
+        </div>  
+        </div>  
      </Container>
-    </div>
+  </div>
 
   );
 }
