732eb58deeea3bd274ae59bf2146f1e4b890cc96
Samantha Thompson <samanthathompson521@gmail.com>
2021-09-30 14:15:00 -0400

    Updated Resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -20,7 +20,7 @@ const AboutMe = () => {
                 <h5>Neurobiology & Physiology graduate searching for an opportunity to grow and discover new technology. </h5>
                 <h5>Throughout my undergraduate education I found myself gaining new experiences in clinical, research, and academia areas of biology. During this, I realized how much of an impact technology can have on healthcare and research!</h5>
                 <h5>I discovered that I loved programming through several of Purdue University's computer science courses. Since graduation, I have dedicated my time to learning even more about the growing technologies of the world.</h5>
-                <h5>Now, I wish to create something of my own and maybe help someone along the way.</h5>
+                <h5>Now, I wish to create something of my own and learn even more aspects of technology.</h5>
                 <div className = "py-4"> 
                   <a href = "/Resume.pdf" target = "_blank"><button type="button" className="btn btn-primary raised btn-block">Resume</button></a>
                 </div>
