56ea09f3933b3ca2f366408fca92f13241e44638
Samantha Thompson <samanthathompson521@gmail.com>
2021-10-03 15:19:13 -0400

    Mobile improvements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,12 +13,12 @@ const Home = () => {
             <h1>Samantha Thompson</h1>
             <h2>Aspiring Software Developer</h2>
             <h3 className = "py-3">Learning about the power of technology and the impact it can have on the world.</h3>
-           
           </div>
-         <span id= 'about'></span>
+          <span id= 'about'></span>
         </Container>
-        
+           
     </div>
+ 
 </div>
   );
 }
